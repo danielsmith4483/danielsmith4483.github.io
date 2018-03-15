d93c940e06bb152b0824d53f332bc3204cff849e
Daniel Smith <danielsmith4483@gmail.com>
2018-03-15 18:41:28 -0500

    Make a quick mockup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import iconGithub from "./Icons/github.svg";
+import iconLinkedin from "./Icons/linkedin.svg";
+import iconInstagram from "./Icons/instagram.svg";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Daniel Smith</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <img
+            src={iconGithub}
+            className="App-social-icon"
+            alt="Github social icon"
+          />
+          <img
+            src={iconLinkedin}
+            className="App-social-icon"
+            alt="Linkedin social icon"
+          />
+          <img
+            src={iconInstagram}
+            className="App-social-icon"
+            alt="Instagram social icon"
+          />
         </p>
+        <p>Software Engineer with a background in Computer Science.</p>
       </div>
     );
   }
