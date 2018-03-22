0c32856b5427e572dd725049a7ed7c72ab8ee26d
Daniel Smith <danielsmith4483@gmail.com>
2018-03-21 21:49:29 -0500

    add some initial content

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,27 @@
-import { h, Component } from 'preact';
-import { Link } from 'preact-router/match';
-import style from './style';
+import { h, Component } from "preact";
+import { Link } from "preact-router/match";
+import style from "./style";
 
 export default class Header extends Component {
-	render() {
-		return (
-			<header class={style.header}>
-				<h1>Preact App</h1>
-				<nav>
-					<Link activeClassName={style.active} href="/">Home</Link>
-					<Link activeClassName={style.active} href="/profile">Me</Link>
-					<Link activeClassName={style.active} href="/profile/john">John</Link>
-				</nav>
-			</header>
-		);
-	}
+  render() {
+    return (
+      <header class={style.header}>
+        <h1>Daniel Smith</h1>
+        <nav>
+          <Link activeClassName={style.active} href="/">
+            About Me
+          </Link>
+          <Link activeClassName={style.active} href="/#resume">
+            Resume
+          </Link>
+          <Link activeClassName={style.active} href="/#projects">
+            Projects
+          </Link>
+          <Link activeClassName={style.active} href="/#contact">
+            Contact
+          </Link>
+        </nav>
+      </header>
+    );
+  }
 }
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,13 +1,20 @@
-import { h, Component } from 'preact';
-import style from './style';
+import { h, Component } from "preact";
+import style from "./style";
 
 export default class Home extends Component {
-	render() {
-		return (
-			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div class={style.home}>
+        <p class={style.card}>
+          Hello! I'm a software engineer with a background in Computer Science.
+          I currently work at the Naval Research Laboratory. I love Node, React,
+          Electron, and weightlifting.
+        </p>
+
+        <p class={style.card}>Resume</p>
+
+        <p class={style.card}>Projects</p>
+      </div>
+    );
+  }
 }
