99db1603f00d43f17d0387d3e29691b4c72cd55c
Daniel Smith <danielsmith4483@gmail.com>
2018-04-01 01:20:54 -0500

    Add some google fonts and webfontloader dependency

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import './style';
-import App from './components/app';
+import "./style";
+import App from "./components/app";
+
+import WebFont from "webfontloader";
+
+WebFont.load({
+  google: {
+    families: ["Space Mono", "Open Sans"]
+  }
+});
 
 export default App;
