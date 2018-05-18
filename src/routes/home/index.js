77c8174f4b5c0128e9379defef9c9a8b857fce78
Daniel Smith <danielsmith4483@gmail.com>
2018-05-17 22:08:01 -0500

    Add open graph meta tags

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,22 +1,19 @@
-import { h, Component } from "preact";
-import style from "./style";
+import { h, Component } from 'preact';
+import style from './style';
 
-import CardsContainer from "../../Containers/CardsContainer";
-import LinksContainer from "../../Containers/LinksContainer";
-
-import HeroImage from "../../Components/HeroImage";
+import CardsContainer from '../../Containers/CardsContainer';
+import LinksContainer from '../../Containers/LinksContainer';
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div class={style.home}>
-        <div class={style.parallax}>
-        </div>
-        <div class={style.contents}>
-          <CardsContainer />
-        </div>
-        <LinksContainer />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div class={style.home}>
+				<div class={style.parallax} />
+				<div class={style.contents}>
+					<CardsContainer />
+				</div>
+				<LinksContainer />
+			</div>
+		);
+	}
 }
