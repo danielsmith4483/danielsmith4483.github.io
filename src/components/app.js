f17815225665f424f1799e1d44f118260b346971
Daniel Smith <danielsmith4483@gmail.com>
2018-05-17 22:10:48 -0500

    Fix an issue with app.js

--- src/components/app.js
+++ src/components/app.js
@@ -11,26 +11,37 @@ if (module.hot) {
 	require('preact/debug');
 }
 
-export default function Application() {
-	return (
-		<div id="app">
-			<Helmet
-				title="Daniel Smith"
-				meta={[
-					{ name: 'description', content: 'I\'m Daniel, a software engineer with a background in Computer Science. I\'m passionate about delivering a good user experience, optimization, and web development.' },
-					{ property: 'og:type', content: 'article' },
-					{ property: 'og:site_name', content: 'danielsmith.io' },
-					{ property: 'og:description', content: 'I\'m Daniel, a software engineer with a background in Computer Science. I\'m passionate about delivering a good user experience, optimization, and web development.' },
-					{ property: 'og:image', content: 'https://image.ibb.co/kEkOnH/pic_png.png' },
-					{ property: 'og:url', content: 'https://danielsmith.io' },
-					{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-					{ name: 'keywords', content: 'danielsmith4483, Daniel Smith, software engineer, computer scientist' }
-				]}
-			/>
-			<Header />
-			<Router onChange={this.handleRoute}>
-				<Home path="/" />
-			</Router>
-		</div>
-	);
+export default class App extends Component {
+
+	/** Gets fired when the route changes.
+	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+	 *	@param {string} event.url	The newly routed URL
+	 */
+	handleRoute = e => {
+		this.currentUrl = e.url;
+	};
+
+	render() {
+		return (
+			<div id="app">
+				<Helmet
+					title="Daniel Smith"
+					meta={[
+						{ name: 'description', content: 'I\'m Daniel, a software engineer with a background in Computer Science. I\'m passionate about delivering a good user experience, optimization, and web development.' },
+						{ property: 'og:type', content: 'article' },
+						{ property: 'og:site_name', content: 'danielsmith.io' },
+						{ property: 'og:description', content: 'I\'m Daniel, a software engineer with a background in Computer Science. I\'m passionate about delivering a good user experience, optimization, and web development.' },
+						{ property: 'og:image', content: 'https://image.ibb.co/kEkOnH/pic_png.png' },
+						{ property: 'og:url', content: 'https://danielsmith.io' },
+						{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+						{ name: 'keywords', content: 'danielsmith4483, Daniel Smith, software engineer, computer scientist' }
+					]}
+				/>
+				<Header />
+				<Router onChange={this.handleRoute}>
+					<Home path="/" />
+				</Router>
+			</div>
+		);
+	}
 }
